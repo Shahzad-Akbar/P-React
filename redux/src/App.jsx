7c6657358c3e8c75cee688237c5dda96b2591dca
Shahzad-Akbar <161068644+Shahzad-Akbar@users.noreply.github.com>
2026-09-13 19:58:26 +0530

    lazy handling

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-
+import { lazy, Suspense, useState } from 'react'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+// import Home from './component/Home.jsx'
+// import About from './component/About.jsx'
+// import Contact from './component/Contact.jsx'
+const Home = lazy(()=> import('./component/Home.jsx'));
+const About = lazy(()=> import('./component/About.jsx'));
+const Contact = lazy(()=> import('./component/Contact.jsx'));
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello Redux</h1>
+      <BrowserRouter>
+      <h1>I am redux</h1>
+      <Link to='/'><button>Home</button></Link>
+      <Link to='about'><button>About</button></Link>
+      <Link to='contact'><button>contact</button></Link>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
